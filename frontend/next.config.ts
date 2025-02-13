92cf847793347c7768bfe9faaa5738e32e46ea57
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2025-02-13 12:03:19 +0300

    reverted the hero section

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,11 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "www.trendai.app" },
-      { protocol: "https", hostname: "cdn.dribbble.com" },
-    ],
-  },
+    domains: ['www.trendai.app'],
+    },
 };
 
 export default nextConfig;
